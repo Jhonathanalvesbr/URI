2948e9edad1558105ad436098c8e2f191cef9a9a
Jhonathanalvesbr <Jhonathanalves_br@hotmail.com>
2018-04-18 10:04:47 -0300

    Create 2484 - Abracadabra.c

---
+++ 2484 - Abracadabra.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main ()
+{
+	char magia[1001] = "";
+	int tamanhomagia = 0, x = 0, i = 0, j = 0;
+	
+	while(scanf("%s", magia) != EOF)
+	{
+		
+		tamanhomagia = strlen(magia);
+		
+		for(tamanhomagia; tamanhomagia > 0; tamanhomagia --)
+		{
+			for(j; j > 0; j--)
+			{
+				printf(" ");
+			}
+			
+			for(x; x < tamanhomagia; x++)
+			{
+				printf("%c", magia[x]);
+			
+				if(x < tamanhomagia-1)
+				{
+					printf(" ");
+				}
+
+			}
+
+			puts("");
+			x = 0;
+			i++;
+			j = i;
+			
+		}
+		
+		j = 0;
+		i = 0;
+
+		puts("");
+	}
+	
+	return 0;
+}
